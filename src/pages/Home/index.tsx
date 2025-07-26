df3ed30d6da5645384e1c334fe81c5a0384c5265
ayay459547 <ayay459547@gmail.com>
2025-07-26 11:17:24 +0800

    feat: 首頁新增說明區

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
 import Addchart from '@mui/icons-material/Addchart'
+import Extension from '@mui/icons-material/Extension'
+import BookmarkAdded from '@mui/icons-material/BookmarkAdded'
+import FlashOn from '@mui/icons-material/FlashOn'
 
 import classes from './Home.module.css'
+import Description from './Description'
 import Dashboard from './Dashboard'
 import Report from './Report'
 import DenseTable from './DataTable'
@@ -14,6 +18,25 @@ export default function Home() {
         <p>with Vite and React</p>
       </h2>
       <p>Welcome to the home page!</p>
+
+      <div className={classes.descriptions}>
+        <Description
+          title="Composable"
+          text="Quickly build your charts with decoupled, reusable React components."
+          icon={<Extension />}
+        />
+        <Description
+          title="Reliable"
+          text="Built on top of SVG elements with a lightweight dependency on D3 submodules."
+          icon={<BookmarkAdded />}
+        />
+        <Description
+          title="Powerful"
+          text="Customize your chart by tweaking component props and passing in custom components."
+          icon={<FlashOn />}
+        />
+      </div>
+
       <Dashboard />
 
       <h2 className={classes.title}>Beautiful charts</h2>
